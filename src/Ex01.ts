21e985849203735f7e34f2238760137c1c07df0f
hdblouro <henrique.louro@terra.com.br>
2025-03-24 20:45:21 -0300

    Alterações Ex 01 e 02

--- src/Ex01.ts
+++ src/Ex01.ts
@@ -1,2 +1,4 @@
 let entrada:string = "12";
-entrada = parseInt(entrada); // entrada já foi tipada como string e paseInt converte string em number/integer
+//entrada = parseInt(entrada); // entrada já foi tipada como string e paseInt converte string em number/integer
+let n:number = parseInt(entrada); 
+console.log(n);
--- src/Ex02.ts
+++ src/Ex02.ts
@@ -1,6 +1,8 @@
 const nros = [11,22,33];
 nros[2] = 0.5;
 nros[3] = 1.5;
+console.log(nros)
 console.log(nros[2]);
 console.log(nros[3]);
-nros = [0.5,0.8,0.1]; // nros é uma constante
+console.log(nros);
+//nros = [0.5,0.8,0.1]; // nros é uma constante
